2e8d235882a9fcdb0055d08cbf7702a14e61fdb6
Clint McMahon <clint@nordicdev.io>
2020-06-08 20:24:50 -0500

    updated css and env files

--- src/HoverPopUp.js
+++ src/HoverPopUp.js
@@ -8,24 +8,38 @@ function HoverPopup(props) {
     let y = props.y;
     if (hoveredFeature) {
         let properties = hoveredFeature.properties;
-        let rep = JSON.parse(properties.REP);
+        if (properties.REP) {
+            let rep = JSON.parse(properties.REP);
 
-        return (
+            return (
+                <div className="hover-popup" style={{ left: x, top: y }}>
+                    <Row>
+                        <Col xs={12}>
+                            <div className="name">{rep.Member}</div>
+                            <div className="value"><span className="font-weight-bold">District: </span> {rep.District}</div>
+                            <div className="value"><span className="font-weight-bold">Party: </span> {rep.Party}</div>
+                            <div className="value"><span className="font-weight-bold">Education: </span> {rep.Education}</div>
+                            <div className="value"><span className="font-weight-bold">Prior experience: </span> {rep["Prior experience"]}</div>
+                            <div className="value"><span className="font-weight-bold">Assumed office: </span> {rep["Assumed office"]}</div>
+                            <div className="value"><span className="font-weight-bold">Residence: </span> {rep.Residence}</div>
+                            <div className="value"><span className="font-weight-bold">Born: </span> {rep.Born}</div>
+                        </Col>
+                    </Row>
+                </div>
+            );
+        }
+        else
+        {
+           return (
             <div className="hover-popup" style={{ left: x, top: y }}>
-                <Row>
-                    <Col xs={12}>
-                        <div className="name">{rep.Member}</div>
-                        <div className="value"><span className="font-weight-bold">District: </span> {rep.District}</div>
-                        <div className="value"><span className="font-weight-bold">Party: </span> {rep.Party}</div>
-                        <div className="value"><span className="font-weight-bold">Education: </span> {rep.Education}</div>
-                        <div className="value"><span className="font-weight-bold">Prior experience: </span> {rep["Prior experience"]}</div>
-                        <div className="value"><span className="font-weight-bold">Assumed office: </span> {rep["Assumed office"]}</div>
-                        <div className="value"><span className="font-weight-bold">Residence: </span> {rep.Residence}</div>
-                        <div className="value"><span className="font-weight-bold">Born: </span> {rep.Born}</div>
-                    </Col>
-                </Row>
-            </div>
-        );
+                    <Row>
+                        <Col xs={12}>
+                            <div className="name">No member assigned</div>
+                        </Col>
+                    </Row>
+                </div>
+           );
+        }
     }
     else {
         return null;
