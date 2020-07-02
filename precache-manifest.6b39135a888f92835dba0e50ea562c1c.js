0b32b96caa7bbd0df5061b6c1dc3cbc58a73531c
Clint McMahon <clint@nordicdev.io>
2020-07-02 13:28:29 -0500

    Updates

---
+++ precache-manifest.6b39135a888f92835dba0e50ea562c1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8f43cc6ce86ff0089901d45494a11e",
+    "url": "/us-congressional-districts/index.html"
+  },
+  {
+    "revision": "e159bad57913aa53c7fc",
+    "url": "/us-congressional-districts/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "0e89eb4ee5849da41758",
+    "url": "/us-congressional-districts/static/css/main.5c2b0d36.chunk.css"
+  },
+  {
+    "revision": "e159bad57913aa53c7fc",
+    "url": "/us-congressional-districts/static/js/2.071abe4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/us-congressional-districts/static/js/2.071abe4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e89eb4ee5849da41758",
+    "url": "/us-congressional-districts/static/js/main.2f8cffee.chunk.js"
+  },
+  {
+    "revision": "8e74c6b02fa48097e1db",
+    "url": "/us-congressional-districts/static/js/runtime-main.aa0367eb.js"
+  }
+]);
